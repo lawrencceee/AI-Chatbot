88a2ffa94695c894b3ff34c7591797a9625d4bae
lawrencceee <fantasy.adoration@gmail.com>
2025-05-08 13:13:05 -0400

    Update app.py

--- chatbot/app.py
+++ chatbot/app.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 openai.api_key = os.getenv("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")
 ## Langsmith tracking
 os.environ["LANGCHAIN_TRACING_V2"]="true"
-os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+langchain_api_key=os.getenv("LANGCHAIN_API_KEY")
 
 ## prompt template
 prompt=ChatPromptTemplate.from_messages(
