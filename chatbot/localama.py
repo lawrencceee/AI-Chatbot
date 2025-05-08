c682608863dc35c441d3fb0ea0c4fa6b7e8048ea
lawrencceee <fantasy.adoration@gmail.com>
2025-05-08 12:28:08 -0400

    Initial commit

---
+++ chatbot/localama.py
@@ -0,0 +1,34 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms import Ollama
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+## Langsmith tracking
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+
+## prompt template
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assitant. Please response to the user queries."),
+        ("user","Question:{question}")
+    ]
+)
+
+## streamlit framework
+
+st.title('Langchain Demo With OPENAI API')
+input_text=st.text_input("Search the topic u want")
+
+# ollama LLAma2 LLM
+llm=Ollama(model="llama2")
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({'question':input_text}))
