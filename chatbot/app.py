2221f2143217eded278fed4a4bd1a218be331d6c
lawrencceee <fantasy.adoration@gmail.com>
2025-05-08 14:12:40 -0400

    Update app.py

--- chatbot/app.py
+++ chatbot/app.py
@@ -8,8 +8,9 @@ import os
 from dotenv import load_dotenv
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
-os.environ["LANGCHAIN_TRACING_V2"]="true"
-langchain_api_key=st.secrets["LANGCHAIN_API_KEY"]
+s.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGCHAIN_API_KEY"]
+os.environ["LANGCHAIN_TRACING_V2"] = st.secrets["LANGCHAIN_TRACING_V2"]
+os.environ["LANGCHAIN_PROJECT"] = st.secrets.get("LANGCHAIN_PROJECT", "DefaultProject")
 
 ## prompt template
 prompt=ChatPromptTemplate.from_messages(
