064dccd92f5fa15ca6263a52764218c888f9191e
lawrencceee <fantasy.adoration@gmail.com>
2025-05-09 21:08:40 -0400

    Update app.py

--- chatbot/app.py
+++ chatbot/app.py
@@ -7,7 +7,7 @@ import streamlit as st
 import os
 from dotenv import load_dotenv
 
-logo_path = "icon.png"
+logo_path = "../icon.png"
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGCHAIN_API_KEY"]
 os.environ["LANGCHAIN_TRACING_V2"] = st.secrets["LANGCHAIN_TRACING_V2"]
